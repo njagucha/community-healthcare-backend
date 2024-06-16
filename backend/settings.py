9accff8e9e6682aba0cac25878b13d82372f8f64
Ndech7 <stephennderitu3@gmail.com>
2024-06-16 06:04:14 +0300

    Updated CORS ORIGINS

--- backend/settings.py
+++ backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://community-healthcare-frontend.onrender.com"]
 
 ROOT_URLCONF = 'backend.urls'
 
